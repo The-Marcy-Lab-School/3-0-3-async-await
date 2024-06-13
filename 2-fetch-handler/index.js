06ead36d4d8c299db77cdf417d12e963b5880008
Ben Spector <111444562+benspector-mls@users.noreply.github.com>
2024-06-13 11:42:52 -0400

    Update index.js

--- 2-fetch-handler/index.js
+++ 2-fetch-handler/index.js
@@ -1,20 +1,37 @@
-const fetchData = async (url) => {
+const fetchData = async (url, options = {}) => {
   try {
-    const response = await fetch(url);
-    if (!response.ok) throw new Error(`fetch failed!: ${response.status}`)
+    const response = await fetch(url, options);
 
-    const jsonData = await response.json();
-    return jsonData;
+    // Throw an error if the response was not 2xx - let the catch statement handle it
+    if (!response.ok) throw new Error(`Fetch failed. ${response.status} ${response.statusText}`)
+
+    // Make sure that the content type of the response is JSON before parsing it
+    // and return a tuple with the data and a null error.
+    const contentType = response.headers.get('content-type');
+    if (contentType !== null && contentType.includes('application/json')) {
+      const jsonData = await response.json();
+      return [jsonData, null]
+    }
+
+    // If the contentType of the response is not JSON, parse it as plain
+    // text and return a tuple with a null error
+    const textData = await response.text();
+    return [textData, null]
   }
   catch (error) {
+    // if there was an error, log it and return a tuple: [data, error]
     console.error(error.message);
-    return null;
+    return [null, error];
   }
 }
 
 const getUsersAsyncAwaitWithHelper = async () => {
-  const jsonData = await fetchData('https://reqres.in/ap/users');
+  const [jsonData, error] = await fetchData('https://reqres.in/api/users');
 
+  if (error) {
+     document.querySelector("#error-message").textContent = error.message
+  }
+  
   // dom manipulation
   const usersList = document.querySelector("#users-list");
   usersList.innerHTML = "";
